fa5c3f2a1f9bb63d58a784262829e8921be97c1e
habib-hb <developerhabib1230@gmail.com>
2024-09-09 18:38:58 +0600

    nice

--- resources/views/livewire/homepage-wire.blade.php
+++ resources/views/livewire/homepage-wire.blade.php
@@ -2,7 +2,7 @@
 
     <nav class="flex justify-between items-center h-[82px] w-[96vw] mx-auto my-2 rounded-lg bg-[#d6e0ec]">
 
-        <img src="{{asset('images/the_logo.png')}}" class=" ml-2 h-[64px]" alt="">
+        <img  src="{{asset('images/the_logo.png')}}" class=" ml-2 h-[64px]" alt="">
 
     </nav>
 
