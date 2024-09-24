73c5a557503f21ca0b0adc2f27c9bc73f758b72d
habib-hb <developerhabib1230@gmail.com>
2024-09-24 11:15:08 +0600

    nice

--- resources/views/livewire/admin-dashboard.blade.php
+++ resources/views/livewire/admin-dashboard.blade.php
@@ -54,7 +54,9 @@
 
 
     {{-- Options --}}
-    <div class="flex justify-center md:justify-between items-center h-[82px] w-[96vw]  md:max-w-[1280px]  md:px-8 mx-auto mt-2 rounded-lg {{session('theme_mode') == 'light' ? 'bg-[#d6e0ec]' : 'bg-[#1e1d1d]'}}  shadow-[0_4px_4px_0_rgba(0,0,0,0.25)]">
+    <div class="flex justify-center md:justify-between items-center  w-[96vw]  md:max-w-[1280px]  md:px-8 mx-auto mt-2 rounded-lg {{session('theme_mode') == 'light' ? 'bg-[#d6e0ec]' : 'bg-[#1e1d1d]'}}  shadow-[0_4px_4px_0_rgba(0,0,0,0.25)]">
+
+        <img src="{{asset('images/banner_headline.gif')}}" alt="">
 
     </div>
 
