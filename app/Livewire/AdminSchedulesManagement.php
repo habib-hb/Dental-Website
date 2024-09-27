56dfdbd2a5d20b8b8054cfe54f0709969fa5c2e5
habib-hb <developerhabib1230@gmail.com>
2024-09-27 17:02:17 +0600

    awesome

---
+++ app/Livewire/AdminSchedulesManagement.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class AdminSchedulesManagement extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin-schedules-management');
+    }
+}
