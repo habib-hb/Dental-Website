38f4ff98850ab1bf0b6997de1e92576c21433f6f
habib-hb <developerhabib1230@gmail.com>
2024-09-27 17:47:17 +0600

    awesome

--- resources/views/livewire/admin-schedules-management.blade.php
+++ resources/views/livewire/admin-schedules-management.blade.php
@@ -30,6 +30,48 @@
 
 
 
+    <main>
+
+        <div class="flex flex-col justify-center items-center mt-8 max-w-[800px] mx-auto px-4 md:px-0">
+
+            <h1 class="text-xl font-semibold text-left md:text-center w-full {{session('theme_mode') == 'light' ? 'text-black' : 'text-white'}}">Schedules Input Panel</h1>
+
+            <p class="text-left md:text-center mt-2 {{session('theme_mode') == 'light' ? 'text-black' : 'text-white'}}">After Adding The Schedules, Click On The “Save” Button On Set The New Schedule List For Appointments. The New Schedule List Will Replace The Old Schedule List. All The Appointments Enlisted From The Old Schedule List Will Be Moved To "Unsettled Appointments" Panel. From There You Can Resettle The Appointments By Clicking The "Settle Appointment" Button And Selecting A New Schedule.</p>
+
+        </div>
+
+
+
+
+
+        {{-- Schedule List --}}
+        <div class="flex flex-col justify-center items-center mt-8 max-w-[800px] mx-auto">
+
+            <h2 class="text-lg font-normal mb-2 {{session('theme_mode') == 'light' ? 'text-black' : 'text-white'}}">Schedule List</h2>
+
+            <p class="text-center py-2 px-8 {{session('theme_mode') == 'light' ? 'bg-[#deeaf8] text-[#484d5f]' : 'bg-[#202329] text-white'}}  mb-3 rounded-lg   shadow-[0_4px_4px_0_rgba(0,0,0,0.25)]  cursor-pointer">11:10 - 12:00 AM</p>
+
+            <p class="text-center py-2 px-8 {{session('theme_mode') == 'light' ? 'bg-[#deeaf8] text-[#484d5f]' : 'bg-[#202329] text-white'}}  mb-3 rounded-lg   shadow-[0_4px_4px_0_rgba(0,0,0,0.25)]  cursor-pointer">11:10 - 12:00 AM</p>
+
+            <p class="text-center py-2 px-8 {{session('theme_mode') == 'light' ? 'bg-[#deeaf8] text-[#484d5f]' : 'bg-[#202329] text-white'}}  mb-3 rounded-lg   shadow-[0_4px_4px_0_rgba(0,0,0,0.25)]  cursor-pointer">11:10 - 12:00 AM</p>
+
+
+            <button class="bg-[#1A579F] text-white font-semibold py-2 px-8 mt-2 rounded-lg shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] hover:scale-110">Save</button>
+
+        </div>
+
+
+
+
+
+
+
+
+    </main>
+
+
+
+
  {{-- Footer Element --}}
  <div class="flex flex-col justify-between items-center py-8 w-[96vw] md:max-w-[1280px]  mx-auto mt-8 rounded-lg {{session('theme_mode') == 'light' ? 'bg-[#d6e0ec]' : 'bg-[#1e1d1d]'}} shadow-[inset_0_4px_4px_rgba(0,0,0,0.25)] mb-2">
 
